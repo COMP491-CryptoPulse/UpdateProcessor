b9dd683b452c275324290aa2d8be45767845cb41
utkn <39ph4f87a@gmail.com>
2021-06-13 17:20:12 +0300

    Disabled prediction temporarily.

--- app/blueprints/update_blueprint.py
+++ app/blueprints/update_blueprint.py
@@ -87,8 +87,8 @@ def update_posts():
     print("Update posts endpoint: Updating within", effective_time_range)
     # groups = list(filter(lambda s: s.startswith("*"), get_all_sources()))
     # create_aggregate_post_impacts(coin_types, groups, effective_time_range)
-    create_aggregate_post_counts(COINS, [], effective_time_range)
-    update_post_impacts(effective_time_range)
+    create_aggregate_post_counts(COINS, SOURCES, effective_time_range)
+    # update_post_impacts(effective_time_range)
     return "ok"
 
 
